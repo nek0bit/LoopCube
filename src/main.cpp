932973f7b45720dcaf6e992cf5edd54ced16377f
nek0bit <hhhyland.belcherrr4@gmail.com>
2020-10-27 21:12:58 -0400

    Remove debug print

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,6 @@ int main() {
 			auto time_after = std::chrono::system_clock::now();
 
 			auto time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(time_after - time_before);
-			std::cout << time_taken.count() << std::endl;
 
 			std::this_thread::sleep_for(std::chrono::milliseconds{frame_del - time_taken.count()});
 		}
