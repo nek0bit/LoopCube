38c460806540c1fd220d5ead691a94a0db8f9976
nek0bit <hhhyland.belcherrr4@gmail.com>
2020-10-27 20:27:10 -0400

    Use std::chrono instead of SDL_GetTicks

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,23 @@ int main() {
 		const int FPS = 60;
 		const int frame_del = 1000 / FPS;
 
-		Uint32 frame;
-		int frame_time;
-
 		Game game{};
 
 		game.init(false);
-
+		
 		while(game.running()) {
-			frame = SDL_GetTicks();
-
+		    auto time_before = std::chrono::system_clock::now();
+		    
 			game.update();
 			game.render();
 			game.event_handler();
 
-			frame_time = SDL_GetTicks() - frame;
+			auto time_after = std::chrono::system_clock::now();
 
-			if (frame_time < frame_del) {
-				SDL_Delay(frame_del - frame_time);
-			}
+			auto time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(time_after - time_before);
+			std::cout << time_taken.count() << std::endl;
+
+			std::this_thread::sleep_for(std::chrono::milliseconds{frame_del - time_taken.count()});
 		}
 		game.free();
 #ifdef __SWITCH__
--- src/main.hpp
+++ src/main.hpp
@@ -1,10 +1,12 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 #include <iostream>
+#include <chrono>
+#include <thread>
 #ifdef __SWITCH__
 #include <switch.h>
 #endif
-#include <SDL2/SDL.h>
+#include "backendincludes.hpp"
 
 #include "game.hpp"
 
