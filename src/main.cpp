2b8803b7a4649e4025e782062d5e33447a12f6c3
nek0bit <hhhyland.belcherrr4@gmail.com>
2020-12-31 13:47:07 -0500

    Main sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -14,42 +14,25 @@ void game_loop() {
 	
 	game.init(false);
 
-#ifdef _SDL2
 	Uint32 frame;
 	int frame_time;
 	
 	while(game.running()) {
 		frame = SDL_GetTicks();
-#else
 		
-	while(game.running()) {
-		auto time_before = std::chrono::system_clock::now();
-#endif
-		
-	game_handles(game);
+        game_handles(game);
 
-#ifdef _SDL2
 	    frame_time = SDL_GetTicks() - frame;
 
 		if (frame_time < frame_del) {
 			SDL_Delay(frame_del - frame_time);
 		}
 	}
-#else
-	    auto time_after = std::chrono::system_clock::now();
-		
-		auto time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(time_after - time_before);
-		
-		std::this_thread::sleep_for(std::chrono::milliseconds{frame_del - time_taken.count()});
-	}
-#endif
 		
 	game.free();
 }
 
 int main(int argc, char* argv[]) {
-	
-		std::cout << "da" << std::endl;
 // Main applet loop for nintendo switch
 #ifdef __SWITCH__	 
 	hidScanInput();
